7999194f50e4340069c2d8d158ec3347a46564e9
vinisi12363 <viniciuspv.si@gmail.com>
2023-05-31 15:19:28 -0300

    fixing bugs

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-<<<<<<< HEAD
-import TimelinePage from "./pages/Timeline.page.js"
-import SignInPage from "./pages/SignIn.page.js"
-import SignUpPage from "./pages/SignUp.page.js"
-import { UserContext } from "./context/UserContext.jsx";
-import { useState } from "react";
-=======
-
 import TimelinePage from "./pages/TimelinePage/Timeline.page.js"
 import SignInPage from "./pages/SignInPage/SignIn.page.js"
-import SignUp from "./pages/SignUpPage/SignUp.page.js";
->>>>>>> main
+import SignUpPage from "./pages/SignUpPage/SignUp.page.js"
+import { UserContext } from "./context/UserContext.jsx";
+import { useState } from "react";
+
+
 
 function App() {
   const [user , setUser] = useState("")
   return (
-<<<<<<< HEAD
+
 
       <UserContext.Provider value={{user, setUser}}>
         <BrowserRouter>
@@ -26,15 +21,7 @@ function App() {
           </Routes>
         </BrowserRouter>
     </UserContext.Provider>
-=======
-    <BrowserRouter>
-      <Routes>
-              <Route path="/" element={<SignInPage />} exact/>
-              <Route path="/sign-up" element={<SignUp />} />
-              <Route path="/timeline" element={<TimelinePage />} />
-      </Routes>
-    </BrowserRouter>
->>>>>>> main
+
   );
 }
 
