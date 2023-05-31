65bf8e18fa27aafc7a90a5a5f08d8cf8d13653b6
winiciuskyutoku <winicius123098@gmail.com>
2023-05-31 12:02:04 -0300

    feat: signup layout and feature

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import TimelinePage from "./pages/Timeline.page.js"
 import SignInPage from "./pages/SignIn.page.js"
-import SignUpPage from "./pages/SignUp.page.js"
+import SignUpPage from "./pages/SignUp/SignUp.page.js"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
               <Route path="/" element={<SignInPage />} exact/>
-              <Route path="/singup" element={<SignUpPage />} />
+              <Route path="/sign-up" element={<SignUpPage />} />
               <Route path="/timeline" element={<TimelinePage />} />
       </Routes>
     </BrowserRouter>
