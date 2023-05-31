b98deadb3bd43b94087863f45fe46087d98da309
Savio Dias Viana <saviodiasviana@gmail.com>
2023-05-31 12:34:45 -0300

    Publicação funcional

--- src/pages/TimelinePage/Timeline.page.js
+++ src/pages/TimelinePage/Timeline.page.js
@@ -1,12 +1,15 @@
+import { useState } from "react"
 import Header from "../../components/Header/Header"
 import Publish from "./Publish"
 import { TimelineContainer } from "./TimelineStyle"
 
 export default function Timeline() {
+  //att será usado para atualizar a timeline
+  const [att, setAtt] = useState(true)
   return (
     <TimelineContainer>
       <Header />
-      <Publish/>
+      <Publish att={att} setAtt={setAtt}/>
     </TimelineContainer>
   )
 }
