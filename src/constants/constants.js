3ed81cccd5d99d6974c9446a2d36633b45d00d34
Savio Dias Viana <saviodiasviana@gmail.com>
2023-06-09 20:40:58 -0300

    Finalização da tela infinita do convidado

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,3 @@
-export const exist = JSON.parse(localStorage.getItem("user")).user_token
+export const exist = JSON.parse(localStorage.getItem("user"))?.user_token
 export const config = { headers: { Authorization: `Bearer ${localStorage.getItem('user_token')}` } }
-export const userImage = JSON.parse(localStorage.getItem("user")).user_photo
+export const userImage = JSON.parse(localStorage.getItem("user"))?.user_photo
